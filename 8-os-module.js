7a3b436118bb77fd3072bc1ad834da9d0dc4f8ba
Denis Romanenko <deromanenko@gmail.com>
2022-04-16 22:13:01 +0400

    initial commit

---
+++ 8-os-module.js
@@ -0,0 +1,14 @@
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(`System uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem() / 1024 / 1024,
+    freemem: os.freemem()
+}
+
+console.log(currentOS)
